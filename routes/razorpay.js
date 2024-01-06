4a113a3c7db3f263ad770da77f5bfc2dc82a8139
Narayan Jha <wakeupcoders@gmail.com>
2024-01-06 18:06:07 +0530

    razorpay payment verification code.

--- routes/razorpay.js
+++ routes/razorpay.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const CustomErrorHandler = require("../services/CustomErrorHandler");
 let razorpayInstance = require('../helpers/razorpay');
+const crypto = require('crypto');
 
 //Create Order in CoinGate
-
-router.post("/", async(req, res) => {
+router.post("/", async(req, res,next) => {
     var options = {
         amount: req.body.amount * 100, // amount in the smallest currency unit
         currency: "INR",
@@ -32,6 +32,28 @@ router.post("/", async(req, res) => {
 
             }
         });
+    } catch (err) {
+        next(err)
+    }
+});
+
+router.post("/verify", async(req, res,next) => {
+    try {
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+        const body = razorpay_order_id + "|" + razorpay_payment_id;
+        const expectedSignature = crypto.createHmac("sha256", "NxHRojWVvhgA6bPQDws247jD").update(body.toString()).digest("hex");
+        const isAuthentic = expectedSignature === razorpay_signature;
+
+        if (isAuthentic) {
+            // Database comes here
+            // await Payment.create({razorpay_order_id,razorpay_payment_id,razorpay_signature,});
+            // res.redirect(`http://localhost:3000/paymentsuccess?reference=${razorpay_payment_id}`);
+            res.status(200).json({success: true,});
+          } 
+          else 
+          {
+            res.status(400).json({success: false,});
+          }
     } catch (err) {
         next(err)
     }
