b818dc3788fdfa3e3b551f3b8b271b44ff8d8d70
Narayan Jha <wakeupcoders@gmail.com>
2024-05-19 00:04:32 +0530

    client improved code.

--- models/Client.js
+++ models/Client.js
@@ -7,7 +7,17 @@ const ClientSchema = new mongoose.Schema({
     clientEmail: { type: String, required: true, unique: true },
     clientPhone: { type: String, required: true },
     clientLogo: { type: String, required: true},
+    clientLogoText: { type: String, required: true},
+    clientIsLogoText: { type: Boolean,  default: false},
     clientGoogleMapIframe: { type: String, required: true},
+    clientAbout: { type: String, required: true},
+    clientStory: { type: String, required: true},
+    clientMission: { type: String, required: true},
+    clientFacebook: { type: String},
+    clientInstagram: { type: String},
+    clientPinterest: { type: String},
+    clientWhatsapp: { type: String},
+    clientLinkedin: { type: String},
 }, { timestamps: true });
 ClientSchema.plugin(paginate);
 module.exports = mongoose.model("Client", ClientSchema);
